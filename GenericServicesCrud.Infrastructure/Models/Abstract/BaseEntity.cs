f6e931b08885a641b0dac1f12a1b957ea4bea328
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-10-21 21:41:59 +0500

    added base entity that will be used to make further entites.

---
+++ GenericServicesCrud.Infrastructure/Models/Abstract/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericServicesCrud.Infrastructure.Models.Abstract
+{
+    public abstract class BaseEntity
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+        public Guid CreatedBy { get; set; }
+        public Guid UpdatedBy { get; set; }
+    }
+}
