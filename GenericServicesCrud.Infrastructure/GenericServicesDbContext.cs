e9b45b2ee14eecaefbebaeffcb6a647a37912abc
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-10-21 21:45:38 +0500

    added the dbcontext. reason for including it here is that we need our db layer to be totally abstracted.

---
+++ GenericServicesCrud.Infrastructure/GenericServicesDbContext.cs
@@ -0,0 +1,34 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using GenericServicesCrud.Infrastructure.Models.Users;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace GenericServicesCrud.Infrastructure
+{
+    public partial class GenericServicesDbContext : IdentityDbContext<User, Role, Guid>
+    {
+        public GenericServicesDbContext()
+        {
+        }
+
+        public GenericServicesDbContext(DbContextOptions<GenericServicesDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //Local
+            //if (!optionsBuilder.IsConfigured)
+            //{
+            //    optionsBuilder.UseSqlServer("Server=.\\;Database=GenericServicesDb;user id=sa;password=open;",
+            //        x => x.MigrationsHistoryTable("__GSMigrationsHistory"));
+            //}
+        }
+    }
+}
