9ad4ff75ef67fb2fcc3dd3adc38bfba562e8502d
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-10-21 21:50:24 +0500

    added core class library which will hold our entire business logic, for now added empty automapper mapping profile.

---
+++ GenericServicesCrud.Core/MappingProfiles/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using AutoMapper;
+
+namespace GenericServicesCrud.Core.MappingProfiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            
+        }
+    }
+}
