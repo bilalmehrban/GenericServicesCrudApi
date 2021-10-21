9f4cf7558a2717fae5aef875a6c5fd3320961e6c
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-10-21 21:43:11 +0500

    identity user inhertied and added more properties to it, also changed the default primary key from int to guid.

---
+++ GenericServicesCrud.Infrastructure/Models/Users/User.cs
@@ -0,0 +1,41 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using GenericServicesCrud.Common.Enum;
+using Microsoft.AspNetCore.Identity;
+
+namespace GenericServicesCrud.Infrastructure.Models.Users
+{
+    public class User : IdentityUser<Guid>
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public override Guid Id
+        {
+            get => base.Id;
+            set => base.Id = value;
+        }
+
+        public override string UserName
+        {
+            get => base.Email;
+            set => base.Email = value;
+        }
+
+        public override string PhoneNumber
+        {
+            get => base.PhoneNumber;
+            set => base.PhoneNumber = value;
+        }
+        public string Name { get; set; }
+        public string FamilyName { get; set; }
+        public UserStatus Status { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+        public DateTimeOffset UpdatedDate { get; set; }
+    }
+}
