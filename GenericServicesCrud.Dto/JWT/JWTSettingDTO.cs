50db61ac2abd6bfb15987dd84a89ec4d56b0ec3f
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-10-21 22:50:45 +0500

    added dto for JWT

---
+++ GenericServicesCrud.Dto/JWT/JWTSettingDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace GenericServicesCrud.DTO.JWT
+{
+    public class JWTSettingDTO
+    {
+        public string SecurityKey { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+    }
+}
