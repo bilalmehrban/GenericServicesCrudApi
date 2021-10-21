bb3ac0f435b6077bea14e08e70470ff4325b4d2f
Bilal Mehrban <17435774+bilalmehrban@users.noreply.github.com>
2021-10-21 21:43:45 +0500

    inherited identityrole to make primary key as guid

---
+++ GenericServicesCrud.Infrastructure/Models/Users/Role.cs
@@ -0,0 +1,12 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Coalition of the Good-Hearted Engineers
+// FREE TO USE AS LONG AS SOFTWARE FUNDS ARE DONATED TO THE POOR
+// ---------------------------------------------------------------
+
+using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace GenericServicesCrud.Infrastructure.Models.Users
+{
+    public class Role : IdentityRole<Guid> { }
+}
